da075511e4d7289f7badbe12900a0025b390eaeb
dannyteofilo <danyel.285309@gmail.com>
2023-05-22 20:42:59 -0600

    feat: the function and the view to find the multiples are implemented

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { SharedModule } from '@shared';
 import { MaterialModule } from '@app/material.module';
@@ -9,7 +13,18 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, SharedModule, FlexLayoutModule, MaterialModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    SharedModule,
+    FlexLayoutModule,
+    MaterialModule,
+    HomeRoutingModule,
+    FormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
